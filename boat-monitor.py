9281e86263541eb5e61277e574d17eda6a94ae93
Severin Lindenmann <github@severin.io>
2024-01-27 13:54:06 +0100

    Update boat-monitor.py

--- boat-monitor.py
+++ boat-monitor.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import bigquery as bq
-import pandas as pd
 
 if __name__ == "__main__":
     st.set_page_config(
@@ -18,38 +17,16 @@ if __name__ == "__main__":
     st.title("👋 Welcome to Analytics Severin")
     st.text("In the sidebar you can find the different pages.")
 
-def mobility():   
+def boat_data():   
     query="""
-SELECT
-  *
-FROM
-  `seli-data-storage.data_storage_1.free_bike_status`
-WHERE
-  DATE(refresh_time) = "2023-03-10"
-  AND TIMESTAMP(crawl_time) = (
-  SELECT
-    crawl_time
-  FROM
-    `seli-data-storage.data_storage_1.free_bike_status`
-  WHERE
-    DATE(refresh_time) = "2023-03-10"
-  ORDER BY
-    crawl_time DESC
-  LIMIT
-    1)
+SELECT * 
+FROM `seli-data-storage.data_storage_1.lora_iot` 
+ORDER BY received_at DESC
+LIMIT 20
 """
     df = bq.query_bigquery_return_df(query, origin='mobility')
 
     return df
 
-df = mobility()
-unique_providers = df['provider_id'].unique()
-col1,col2,col3,col4 = st.columns(4)
-col1.metric('Unique Providers',len(unique_providers))
-col2.metric('Bikes and Scooters',len(df['bike_id'].unique()))
-col3.metric('Are reserved',len(df[df['is_reserved']]))
-col4.metric('Are Disabled',len(df[df['is_disabled']]))
-
-st.bar_chart(df.groupby('provider_id')['bike_id'].count())
-st.dataframe(unique_providers)
+df = boat_data()
 st.dataframe(df)
